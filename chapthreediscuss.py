e2a9fbc9ce5815525af413b07c69a4cdd9ebdd90
cmotek <38844285+cmotek@users.noreply.github.com>
2019-12-02 20:43:11 -0800

    chapter_3_discussion

---
+++ chapthreediscuss.py
@@ -0,0 +1,85 @@
+import math
+
+#x = 4.0 / 10 + 3.5 * 2
+#print(x)
+
+#x = 10 % 4 + 6 / 2
+#print(x)
+
+#x = abs(4 - 20 // 3) ** 3
+#print(x)
+
+#x = math.sqrt(4.5 - 5.0) + 7 * 3
+#print (x)
+#MATH DOMAIN ERROR
+
+#x = 3 * 10 // 3 + 10 % 3
+#print(x)
+
+#x = 3 ** 3
+#print(x)
+
+#y = (3 + 4) * 5
+#print(y)
+
+
+#n = int(input("Please provide a number: "))
+#x = (n * (n-1)) / 2
+#print(x)
+
+#r = int(input("Please provide a number: "))
+#x = 4 * math.pi * (r ** 2)
+#print(x)
+
+#a = int(input("Please provide a: "))
+#r = int(input("Please provide r: "))
+#s = int(input("Please provide s: "))
+#x = math.sqrt((a * (math.cos(r) ** 2)) + (a * (math.sin(s) ** 2)))
+#print(x)
+
+#x = int(input("Please provide x: "))
+#y = int(input("Please provide y: "))
+#r = ((y ** 2) - (y ** 1))/((x ** 2) - (x ** 1))
+#print(r)
+
+#for i in range(5):
+    #print(i)
+
+#for i in range(3, 10):
+    #print(i)
+
+#for i in range(4, 13, 2):
+#    print(i)
+#print()
+
+#for i in range(15, 5, -2):
+#    print(i)
+#print()
+
+#for i in range(5, 3):
+#    print(i)
+#print()
+
+#for i in range(1, 11):
+#    print(i * i)
+
+#for i in range[1,3,5,7,9]:
+#    print(i, ":", i**3)
+#print(i)
+#RANGE IS BUILT IN IMMUTABLE FUNCTION
+
+#x = 2
+#y = 10
+#for j in range(0, y, x):
+#    print(j, end="")
+#    print(x + y)
+#print("done")
+
+#ans = 0 
+#for i in range(1, 11):
+#    ans = ans + i * i
+#    print(i)
+#print(ans)
+
+#round(314.159265, -1)
+
