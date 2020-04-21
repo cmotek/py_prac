2be99b465b3c66ba3f36c26f93d3ccd1dd20b2a7
cmotek <38844285+cmotek@users.noreply.github.com>
2020-04-20 22:05:16 -0700

    chapeightwork

---
+++ chapeightdiscuss.py
@@ -0,0 +1,38 @@
+
+
+
+
+def main():
+
+    #n = int(input("Give me a number to sum to:")) 
+    #total = 0
+    #t = 0
+    #while t <= n:
+    #    print(t)
+    #    t = t + 1
+    #    total = total + t
+    #print(total)
+
+
+    #n = int(input("Give me a number to sum odds to:"))
+    #total = 0
+    #count = 0
+    #while ((2 * count) - 1) <= n:
+    #    runningt = ((2 * count) - 1)
+    #    total = total + runningt
+    #    count = count + 1
+    #    print(runningt)
+    #    print(total)
+
+    ninetynum = 0
+
+    
+
+
+
+
+
+
+
+
+main()
