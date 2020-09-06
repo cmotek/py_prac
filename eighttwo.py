cef0f46e8214afb9a43552deede47805d9a60a29
cmotek <38844285+cmotek@users.noreply.github.com>
2020-09-05 19:01:47 -0700

    eighttwo

--- eighttwo.py
+++ eighttwo.py
@@ -2,39 +2,38 @@
 
 def main():
 
-    tempinit = 50
-    veloinit = 0
+    tempinit = -20
+    veloinit = 50
     def windy(temp,velo):
-        while velo != 55:
-            windchill = 35.74 + (0.6215 * temp) - (35.75 * (velo ** .16)) + ((0.4275 * temp) * (velo ** 0.16))
-            windchill = str(windchill)
-            print(windchill[0:4], end=" ")
-            print(velo)
-            velo = velo + 5
 
-    print(tempinit, windy(tempinit, veloinit))
+
+        
+        while velo != -5:
+            chills = []
+            while temp != 70:
+                windchill = str(35.74 + (0.6215 * temp) - (35.75 * (velo ** .16)) + ((0.4275 * temp) * (velo ** 0.16)))[0:6]
+                chills.append(windchill)
+                temp = temp + 10
+            print(velo,chills)
+            temp = -20
+            velo = velo - 5
+
+    
+              
+        
+        
+            
+
             
 
     print("This program computes windchill")
     print()
     
-    print("Temperature    " + "     " + "    Windchills")
+    print("Wind Speed    " + "     " + "    Windchills")
 
-    #print(str(tempinit) + "              " +  windy(tempinit,veloinit))
-    
+    windy(tempinit, veloinit)
 
-
-
-
-
-
-
-
-
-
-
-
-
+    print("Temp: -20       -10        0        10        20        30        40        50        60")
 
 
 
