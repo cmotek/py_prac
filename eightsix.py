f75356b17afb09db16ef647bb9cd1aa7f7760593
cmotek <38844285+cmotek@users.noreply.github.com>
2020-09-08 21:02:25 -0700

    eightsixwork

--- eightsix.py
+++ eightsix.py
@@ -2,33 +2,54 @@ import math
 
 def main():
 
-    print("Is this number prime?")
+    print("What numbers less than my number are prime?")
 
 
     startnumber = eval(input("Enter a number greater than 2:"))
 
-    counter = 2
+    primecount = 2
 
     primelist = []
 
-    while counter <= math.sqrt(startnumber):
+    for i in range(startnumber):
+
+        print(i)
+        while primecount <= math.sqrt(i):
+
+            if i % primecount != 0:
+                primecount = primecount + 1
+            elif i % primecount == 0:
+                print("This is not a prime number!")
+                pass
+
+        if primecount > math.sqrt(i):
+            primelist.append(i)
+            print(primelist)
+
+                
+
+   # counter = 2
+
+   # while counter <= math.sqrt(startnumber):
 
 
-        if startnumber % counter != 0:
-            counter = counter + 1
-            primelist.append(counter)
-        elif startnumber % counter == 0:
-            print("This is not a prime number at all!")
-            break
-        
+   #     if startnumber % counter != 0:
+   #         counter = counter + 1
+   #     elif startnumber % counter == 0:
+   #         print("This is not a prime number at all!")
+   #         break
             
-    if counter > math.sqrt(startnumber):
-        print("Holy moly it's a prime number")
+   # if counter > math.sqrt(startnumber):
+   #     print("Holy moly it's a prime number")
 
     
-    print("These are the primes below your starting number: ", primelist)
+
+      
+
+            
 
 
+ 
 
 
 main()
